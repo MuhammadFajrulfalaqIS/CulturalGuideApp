e411a9dd1e91711496673ad9cf5e9018562c5af1
iqbalrsyd <iqbalhidayatrasyad@mail.ugm.ac.id>
2025-10-31 19:51:37 +0700

    inisiasi projek

---
+++ app/src/main/java/com/example/cultural_navigation_papb/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.cultural_navigation_papb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cultural_navigation_papb.ui.theme.CulturalnavigationpapbTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CulturalnavigationpapbTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CulturalnavigationpapbTheme {
+        Greeting("Android")
+    }
+}
